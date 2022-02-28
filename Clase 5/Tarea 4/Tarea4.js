f5d778d911cfd50258822ad1b72cc4ef5613019f
SebastianFurnier <sfurnier@hotmail.com>
2022-02-28 17:40:23 -0300

    Tarea 1 borrada

--- Clase 5/Tarea 4/Tarea4.js
+++ Clase 5/Tarea 4/Tarea4.js
@@ -1,3 +1,12 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+
 let numeros = document.querySelectorAll('li')
 let arrayNumeros = []
 
@@ -6,6 +15,7 @@ for(let i=0; i<numeros.length; i++){
 }
 
 document.querySelector("#calcular").onclick = function(){
+
     let contador = 0
     for(let i=0; i<arrayNumeros.length; i++){
         contador = (contador + Number(arrayNumeros[i]))
@@ -14,6 +24,7 @@ document.querySelector("#calcular").onclick = function(){
     let promedio = document.querySelector("#promedio")
     promedio.textContent = promedio.textContent + contador
 
+    //Esta función la busque en el MDN
     arrayNumeros.sort(function(a, b) {
         return a - b
       })
@@ -22,8 +33,8 @@ document.querySelector("#calcular").onclick = function(){
     numeroPequenio.innerText = numeroPequenio.innerText + arrayNumeros[0]
 
     let numeroGrande = document.querySelector("#numeroGrande")
-    let final = arrayNumeros.length
-    numeroGrande.innerText = numeroGrande.innerText + arrayNumeros[final - 1]
+    numeroGrande.innerText = numeroGrande.innerText + arrayNumeros[arrayNumeros.length - 1]
 
     
+    document.querySelector("#calcular").disabled = true
 }
